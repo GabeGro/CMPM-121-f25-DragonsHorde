72f8e96ec2f3ee128f35194d3c749c22b8821848
GabeGro <ggroenwo@ucsc.edu>
2025-10-12 21:47:56 +0000

    step 1 complete: added clickable button w/ typescript

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+//step 1: added clickable button to gain treasure
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>Dragon's Horde</h1>
+  <p>Treasure: <span id = "treasure">0</span></p>
+  <button id = "increment">Take Treasure</button>
 `;
