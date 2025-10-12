13b8be45f5e21eeda4c4a2852f524e563a77c92f
GabeGro <ggroenwo@ucsc.edu>
2025-10-12 21:56:29 +0000

    //step 2 complete: button updates treasure counter via even listener

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import "./style.css";
 
+let treasure: number = 0;
+
 //step 1: added clickable button to gain treasure
 document.body.innerHTML = `
   <h1>Dragon's Horde</h1>
   <p>Treasure: <span id = "treasure">0</span></p>
   <button id = "increment">Take Treasure</button>
 `;
+
+const treasureButton = document.getElementById("increment")!;
+const treasureCounter = document.getElementById("treasure")!;
+
+//updates treasure counter on button clicked
+treasureButton.addEventListener("click", () => {
+  treasure++;
+  treasureCounter.textContent = Math.floor(treasure).toString();
+});
