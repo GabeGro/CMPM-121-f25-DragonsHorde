e0639d136d48b308695811612fec2cb5b838ff11
GabeGro <ggroenwo@ucsc.edu>
2025-10-12 22:57:43 +0000

    step 5 complete: added a second button to recruit followers when the user has enough treasure

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,35 @@
 import "./style.css";
 
 let treasure: number = 0;
-const followers: number = 1;
+let followers: number = 1;
 
 //step 1: added clickable button to gain treasure
 document.body.innerHTML = `
   <h1>Dragon's Horde</h1>
   <p>Treasure: <span id = "treasure">0</span></p>
-  <p>Treasure: <span id = "treasure">1</span></p>
+  <p>Followers: <span id = "followers">1</span></p>
   <button id = "increment">Take Treasure</button>
+  <button id = "recruit">Recruit Followers (-10 treasure)</button>
 `;
 
 const treasureButton = document.getElementById("increment")!;
 const treasureCounter = document.getElementById("treasure")!;
+const followersButton = document.getElementById("recruit") as HTMLButtonElement;
+const followersCounter = document.getElementById("followers")!;
+
+followersButton.disabled = true;
 
 //updates treasure counter on button clicked
 treasureButton.addEventListener("click", () => {
   treasure++;
   treasureCounter.textContent = Math.floor(treasure).toString();
+});
+
+//gain follower for 10 treasure
+followersButton.addEventListener("click", () => {
+  followers++;
+  treasure -= 10;
+  followersCounter.textContent = Math.floor(followers).toString();
 });
 
 //get start time
@@ -31,6 +43,13 @@ function offerings(currentTime: number) {
   treasure += followers * secTime;
   treasureCounter.textContent = Math.floor(treasure).toString();
   lastTime = currentTime;
+
+  //disable followers button
+  if (treasure >= 10) {
+    followersButton.disabled = false;
+  } else {
+    followersButton.disabled = true;
+  }
 
   //loop back
   requestAnimationFrame(offerings);
